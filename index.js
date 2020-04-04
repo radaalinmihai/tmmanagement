616a3f1b58d3ee87d7d83b470c32f32b8bfd0271
DontStealMyCodePls <n3gatyv95@gmail.com>
2020-04-04 19:18:09 +0300

    Added a background job
    Added a background job that checks if it is midnight and if it is it should reset all chores to undone

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import BackgroundFetch from 'react-native-background-fetch';
+import {getItem, removeItem, storeItem} from './utils';
+import moment from 'moment';
 
+BackgroundFetch.configure(
+  {
+    minimumFetchInterval: 60,
+    stopOnTerminate: false,
+    startOnBoot: true,
+    requiredNetworkType: BackgroundFetch.NETWORK_TYPE_NONE,
+    enableHeadless: true,
+  },
+  async () => MidnightCheck(),
+  (error) => console.warn(error),
+);
+
+let MidnightCheck = async (event) => {
+  let taskId = event.taskId;
+  const chores = await getItem('@chores');
+
+  if (chores.length > 0) {
+    if (moment().diff(moment().endOf('day')) >= 0)
+      chores.map((chore) => (chore.done ? {...chore, done: false} : chore));
+    await removeItem('@chores');
+    await storeItem('@chores', chores);
+  }
+  console.log('Checked. Changes should have been made if it was the case');
+  BackgroundFetch.finish(taskId);
+};
+
+BackgroundFetch.registerHeadlessTask(MidnightCheck);
 AppRegistry.registerComponent(appName, () => App);
--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {FlatList, AppState} from 'react-native';
+import {FlatList} from 'react-native';
 import NoChoresText from './noChoresText';
 import {getItem, storeItem, removeItem} from '../utils';
 import ChoreItem from './choreItem';
 import {RNToasty} from 'react-native-toasty';
 import {CancelNotification} from './services/LocalPushController';
+import moment from 'moment';
 
 export default class HomeScreen extends React.Component {
   state = {
     chores: [],
-    appState: AppState.currentState,
   };
   getChores = async () => {
     const chores = await getItem('@chores');
@@ -47,20 +47,15 @@ export default class HomeScreen extends React.Component {
         duration: 0,
       });
   };
-  componentDidMount = async () => {
-    AppState.addEventListener('change', (state) =>
-      this.setState({appState: state}),
-    );
-    this.checkScreenState();
-  };
+  componentDidMount = () => this.checkScreenState();
+
   checkScreenState = () =>
     this.props.navigation.addListener(
       'didFocus',
       async () => await this.getChores(),
     );
   render() {
-    const {chores, appState} = this.state;
-    console.log(appState);
+    const {chores} = this.state;
     return (
       <React.Fragment>
         {chores.length > 0 ? (
